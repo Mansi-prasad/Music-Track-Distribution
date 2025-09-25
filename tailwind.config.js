3afbc49d53aa9b534b163d871035dcf52bfc4080
Mansi-prasad <mansi09852@gmail.com>
2025-09-25 16:36:12 +0530

    fix dark/light mode functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ export default {
   darkMode: "class",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
